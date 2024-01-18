c68ce3915a84ac764879fc2fc1148d1f6cd78e19
Derek Paul <derek.paul@brilliant360.net>
2024-01-18 09:49:22 -0500

    testing

---
+++ index.js
@@ -0,0 +1,69 @@
+window.onload = async () => {
+  let posPrinterInfo = [
+    { text: '' },
+    {
+      text: '8XA4SA45',
+      size: {
+        width: 1,
+        height: 1
+      },
+      style: 'bu',
+      align: 'ct'
+    },
+    { text: '' },
+    {
+      text: "RPO",
+      size: {
+        width: 0,
+        height: 1
+      },
+      style: 'n'
+    },
+    { text: '' },
+    {
+      text: "Hola mundo",
+      size: {
+        width: 0,
+        height: 0
+      }
+    },
+    { text: "Vanti" },
+    { text: '' },
+    { text: "15/06/15" },
+    { text: '' },
+    { text: '' },
+    { text: '' }
+  ]
+
+  let posPrinterSetup = {};
+  let electronPrinter = window.ElectronPrinter || window.ipcRenderer ? window.ipcRenderer.ElectronPrinter : null
+  console.log(posPrinterInfo, posPrinterSetup,"posPrinterSetup");
+
+  if (electronPrinter) {
+    if (typeof window.ipcRenderer.ElectronPrinter === 'function') {
+      try {
+        await electronPrinter({
+          mode: 'printURL',
+          url,
+          optsPrinter: {
+            silent: true,
+            printBackground: false,
+            color: false,
+            margin: {
+              marginType: 'printableArea'
+            },
+            landscape: false,
+            scaleFactor: 1
+          },
+          posPrinter: {
+            posPrinterInfo,
+            posPrinterSetup: posPrinterSetup
+          }
+        });
+      } catch (error) {
+        console.error('Error when print ticket', error);
+        console.log(error);
+      }
+    } else console.log('ElectronPrinter not exist');
+  } else console.log('ElectronPrinter not exist');
+};
