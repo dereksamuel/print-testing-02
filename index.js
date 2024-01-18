e0f61ce10468e55e6771f8328f57fa64aed30d75
Derek Paul <derek.paul@brilliant360.net>
2024-01-18 09:57:35 -0500

    testing

--- index.js
+++ index.js
@@ -1,69 +1,73 @@
-window.onload = async () => {
-  let posPrinterInfo = [
-    { text: '' },
-    {
-      text: '8XA4SA45',
-      size: {
-        width: 1,
-        height: 1
+window.onload = () => {
+  document.querySelector('#to-print').addEventListener('click', async () => {
+    let posPrinterInfo = [
+      { text: '' },
+      {
+        text: '8XA4SA45',
+        size: {
+          width: 1,
+          height: 1
+        },
+        style: 'bu',
+        align: 'ct'
       },
-      style: 'bu',
-      align: 'ct'
-    },
-    { text: '' },
-    {
-      text: "RPO",
-      size: {
-        width: 0,
-        height: 1
+      { text: '' },
+      {
+        text: "RPO",
+        size: {
+          width: 0,
+          height: 1
+        },
+        style: 'n'
       },
-      style: 'n'
-    },
-    { text: '' },
-    {
-      text: "Hola mundo",
-      size: {
-        width: 0,
-        height: 0
-      }
-    },
-    { text: "Vanti" },
-    { text: '' },
-    { text: "15/06/15" },
-    { text: '' },
-    { text: '' },
-    { text: '' }
-  ]
-
-  let posPrinterSetup = {};
-  let electronPrinter = window.ElectronPrinter || window.ipcRenderer ? window.ipcRenderer.ElectronPrinter : null
-  console.log(posPrinterInfo, posPrinterSetup,"posPrinterSetup");
-
-  if (electronPrinter) {
-    if (typeof window.ipcRenderer.ElectronPrinter === 'function') {
-      try {
-        await electronPrinter({
-          mode: 'printURL',
-          url,
-          optsPrinter: {
-            silent: true,
-            printBackground: false,
-            color: false,
-            margin: {
-              marginType: 'printableArea'
+      { text: '' },
+      {
+        text: "Hola mundo",
+        size: {
+          width: 0,
+          height: 0
+        }
+      },
+      { text: "Vanti" },
+      { text: '' },
+      { text: "15/06/15" },
+      { text: '' },
+      { text: '' },
+      { text: '' }
+    ]
+  
+    let posPrinterSetup = {};
+    let electronPrinter = window.ElectronPrinter || window.ipcRenderer ? window.ipcRenderer.ElectronPrinter : null
+    console.log(posPrinterInfo, posPrinterSetup,"posPrinterSetup");
+    let url = `/toprint.html`;
+    console.log('To print', url);
+  
+    if (electronPrinter) {
+      if (typeof window.ipcRenderer.ElectronPrinter === 'function') {
+        try {
+          await electronPrinter({
+            mode: 'printURL',
+            url,
+            optsPrinter: {
+              silent: true,
+              printBackground: false,
+              color: false,
+              margin: {
+                marginType: 'printableArea'
+              },
+              landscape: false,
+              scaleFactor: 1
             },
-            landscape: false,
-            scaleFactor: 1
-          },
-          posPrinter: {
-            posPrinterInfo,
-            posPrinterSetup: posPrinterSetup
-          }
-        });
-      } catch (error) {
-        console.error('Error when print ticket', error);
-        console.log(error);
-      }
+            posPrinter: {
+              posPrinterInfo,
+              posPrinterSetup: posPrinterSetup
+            }
+          });
+        } catch (error) {
+          console.error('Error when print ticket', error);
+          console.log(error);
+        }
+      } else console.log('ElectronPrinter not exist');
     } else console.log('ElectronPrinter not exist');
-  } else console.log('ElectronPrinter not exist');
+  })
 };
